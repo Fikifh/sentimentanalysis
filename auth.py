1ee95df1aec35b7c9b7d81e59b8adaab8f1249ca
Fikifh <33621962+Fikifh@users.noreply.github.com>
2019-04-13 22:58:08 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,10 +1,10 @@
 class TwitterAuth:
 	# Go to http://dev.twitter.com and create an app. 
 	# The consumer key and secret will be generated for you after
-	consumer_key="tj9E6a677gjmuzpQHbzvGtfu1"
-	consumer_secret="a2Vag5JDMmP3zaDM9QstarIbFXerHsb6LhM55MTv354yC7Nd1i"
+	consumer_key=""
+	consumer_secret=""
 
 	# After the step above, you will be redirected to your app's page.
 	# Create an access token under the the "Your access token" section
-	access_token="1081116821263048704-kYWCnSij3X76WFkPhWkCoZFryT40og"
-	access_token_secret="eRlw7DNcIv1SYD5t3KaNwKprmQXEiQK8xAfKK1ITzJL6H"
+	access_token=""
+	access_token_secret=""
